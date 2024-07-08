c39b9e65f6ef3e68e6126a512e5efc6640e27f6d
Ryan Zheng <nogrethumphrey@gmail.com>
2024-07-08 06:06:10 +0200

    increase minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="repository_mongodb",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(where="repository_mongodb"),
     package_dir={"": "repository_mongodb"},
     install_requires=[
